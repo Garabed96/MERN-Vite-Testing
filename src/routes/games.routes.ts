baff8e2845149189deec69852395e0dab090665f
Garo <garonazarian09@gmail.com>
2023-09-03 15:58:32 +0700

    eslint changes  | get route

--- src/routes/games.routes.ts
+++ src/routes/games.routes.ts
@@ -1,6 +1,6 @@
 // External Dependencies
 import express, { type Request, type Response } from 'express'
-import { ObjectId } from 'mongodb'
+// import { ObjectId } from 'mongodb'
 import { collections } from '../services/database.service'
 import type Game from '../models/game'
 // Global Config
@@ -11,10 +11,13 @@ gamesRouter.use(express.json())
 gamesRouter.get('/', async (_req: Request, res: Response) => {
    try {
       const games = (await collections.games.find({}).toArray()) as Game[]
-
       res.status(200).send(games)
    } catch (error) {
-      res.status(500).send(error.message)
+      if (error instanceof Error) {
+         res.status(500).send(error.message)
+      } else {
+         res.status(500).send('An unknown error occurred')
+      }
    }
 })
 // POST
